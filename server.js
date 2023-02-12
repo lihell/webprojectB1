c4339f144a36e26ff60a145f92b2f0c66a7b7a6f
lihell <lhellwage@t-h.de>
2023-02-12 10:21:47 +0100

    aufgabe 2 für HomePage und Menu

---
+++ server.js
@@ -0,0 +1,11 @@
+/*server.js*/
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 6000;
+
+const server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+})

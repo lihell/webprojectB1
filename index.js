23d31e77b222d9be49ab2cba477bdcc365fd07c0
lihell <lhellwage@t-h.de>
2023-03-13 20:00:52 +0100

    added box for most visited sites with ordered list

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ const path = require('path');
 const router = express.Router();
 const app = express();
 
-
+let homePageCount = 0;
+let aboutMeCount = 0;
+let menuCount = 0;
+let reservationCount = 0;
 
 
 router.get('/', function(req, res) {
+    homePageCount += 1;
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.send(homePageCount)
     if (true) {
         const userId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         document.cookie = ('userId', userId, { maxAge: 900000, httpOnly: true });
     }
 });
 router.get('/about', function(req, res) {
+    aboutMeCount += 1;
     res.sendFile(path.join(__dirname + '/html/about_me.html'));
+    res.send(aboutMeCount)
 });
 router.get('/menu', function(req, res) {
+    menuCount += 1;
     res.sendFile(path.join(__dirname + '/html/menu.html'));
+    res.send(menuCount)
 });
 router.get('/reservations', function(req, res) {
+    reservationCount += 1;
     res.sendFile(path.join(__dirname + '/html/reservations.html'));
+    res.send(reservationCount)
 });
 
 app.use(express.static(__dirname + ''));

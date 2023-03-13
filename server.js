ee28959edf3a98f105756ec7402b270e7314d8c8
Justus Marx <justus.marx@gmx.de>
2023-03-13 20:45:33 +0100

    refactoring

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 const fs = require('fs');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(cookieParser());app.use("/public", express.static("public"));
+app.use(cookieParser());
+app.use("/public", express.static("public"));
 app.get("/", (_, res) => res.redirect("/public/"));
 
 let sessions = [];
@@ -25,8 +26,6 @@ app.get("/getCommentsMain", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/commentsMain.json', (err, commentData) => {
         const jsonComment = JSON.parse(commentData)
-        console.log("Get request made")
-        console.log(jsonComment)
         res.send(jsonComment)
     });
 })
@@ -36,8 +35,6 @@ app.get("/getCommentsVorspeise", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/commentsVorspeise.json', (err, commentData) => {
         const jsonComment = JSON.parse(commentData)
-        console.log("Get request made")
-        console.log(jsonComment)
         res.send(jsonComment)
     });
 })
@@ -47,8 +44,6 @@ app.get("/getCommentsHauptspeise", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/commentsHauptspeise.json', (err, commentData) => {
         const jsonComment = JSON.parse(commentData)
-        console.log("Get request made")
-        console.log(jsonComment)
         res.send(jsonComment)
     });
 })
@@ -57,8 +52,6 @@ app.get("/getCommentsNachspeise", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/commentsNachspeise.json', (err, commentData) => {
         const jsonComment = JSON.parse(commentData)
-        console.log("Get request made")
-        console.log(jsonComment)
         res.send(jsonComment)
     });
 })
@@ -68,8 +61,6 @@ app.get("/getFav", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/favorites.json', (err, favData) => {
         const jsonFav = JSON.parse(favData)
-        console.log("Mein Get request made")
-        console.log(jsonFav)
         res.send(jsonFav)
     });
 })
@@ -78,38 +69,30 @@ app.post("/fav", (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "*");
     const data = req.body;
-    console.log(data);
     fs.readFile(__dirname + "/ressources/favorites.json", (err, fileData) => {
         if (err) throw err;
         let existing = false;
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(fileData);
 
-        console.log("Hier sind meine LOGS!!!!!!!" + data.userID)
-
-        console.log(req.body)
-
         existingData.forEach(object => {
-            if (object.userID === data.userID && object.buttonID === data.buttonID){
+            if (object.userID === data.userID && object.buttonID === data.buttonID) {
                 object.isFavNow = !(object.isFavNow)
                 existing = true;
             }
-            console.log(object.userID + "    " + object.buttonID)
         })
         // Merge the existing data with the new data received in the request
         let newData
-        if (!existing){
+        if (!existing) {
             newData = [...existingData, req.body];
         } else {
             newData = existingData;
         }
         existing = false;
-        //console.log(newData)
 
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/favorites.json", JSON.stringify(newData), (err) => {
             if (err) throw err;
-            console.log('Data written to file');
         });
     });
 });
@@ -124,16 +107,8 @@ app.post("/postCommentsMain", (req, res) => {
 
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(fileData);
-
-        console.log(existingData)
-
-        console.log(req.body)
-
         // Merge the existing data with the new data received in the request
         const newData = [...existingData, req.body];
-
-        console.log(newData)
-
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/commentsMain.json", JSON.stringify(newData), (err) => {
             if (err) throw err;
@@ -149,18 +124,11 @@ app.post("/postCommentsVorspeise", (req, res) => {
     // Read the existing data from the file
     fs.readFile(__dirname + "/ressources/commentsVorspeise.json", (err, fileData) => {
         if (err) throw err;
-        console.log("im going into the post request")
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(fileData);
 
-        console.log(existingData)
-
-        console.log(req.body)
-
         // Merge the existing data with the new data received in the request
         const newData = [...existingData, req.body];
-
-        console.log(newData)
 
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/commentsVorspeise.json", JSON.stringify(newData), (err) => {
@@ -181,14 +149,8 @@ app.post("/postCommentsHauptspeise", (req, res) => {
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(fileData);
 
-        console.log(existingData)
-
-        console.log(req.body)
-
         // Merge the existing data with the new data received in the request
         const newData = [...existingData, req.body];
-
-        console.log(newData)
 
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/commentsHauptspeise.json", JSON.stringify(newData), (err) => {
@@ -209,14 +171,8 @@ app.post("/postCommentsNachspeise", (req, res) => {
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(fileData);
 
-        console.log(existingData)
-
-        console.log(req.body)
-
         // Merge the existing data with the new data received in the request
         const newData = [...existingData, req.body];
-
-        console.log(newData)
 
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/commentsNachspeise.json", JSON.stringify(newData), (err) => {
@@ -231,8 +187,6 @@ app.get("/getReservations", (req, res) => {
     res.header("Access-Control-Allow-Headers", "*");
     fs.readFile(__dirname + '/ressources/reservations.json', (err, reservationData) => {
         const jsonComment = JSON.parse(reservationData)
-        console.log("Get request made")
-        console.log(jsonComment)
         res.send(jsonComment)
     });
 })
@@ -247,14 +201,8 @@ app.post("/postReservation", (req, res) => {
         // Parse the JSON data into a JavaScript object
         const existingData = JSON.parse(reservationData);
 
-        console.log(existingData)
-
-        console.log(req.body)
-
         // Merge the existing data with the new data received in the request
         const newData = [...existingData, req.body];
-
-        console.log(newData)
 
         // Write the updated data back to the file
         fs.writeFile(__dirname + "/ressources/reservations.json", JSON.stringify(newData), (err) => {
@@ -280,7 +228,7 @@ app.post("/login", (req, res) => {
 
     sessions.push(`${sessionId}`);
 
-    res.cookie("token", sessionId, { maxAge: 5 * 60 * 1000 });
+    res.cookie("token", sessionId, {maxAge: 5 * 60 * 1000});
     res.redirect("/secure");
 });
 
@@ -292,10 +240,8 @@ app.get("/logout", (req, res) => {
 });
 
 app.all("/secure", (req, res, next) => {
-    console.log("sdasdasdasds")
     res.header('Access-Control-Allow-Origin', '*');
     if (req.cookies.token === undefined) {
-        console.log("sds")
         res.redirect("/error");
         return;
     }
